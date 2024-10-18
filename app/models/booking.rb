901312671b8ac33dc7d1038f5fcb619d3726cebf
Henri Bellet <henri.clau.bellet@gmail.com>
2024-10-18 21:32:05 +0200

    booking created

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,13 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :garment
   belongs_to :reviews
+
+  validates :start_date, :end_date, :total_price, presence: true
+  validate :valid_date
+
+  def valid_date
+    if :end_date < :start_date
+      errors.add(:end_date, "Invalid dates. Must be AFTER start date.")
+    end
+  end
 end
